058e54426c9f498e1406a7b51a6c96843ac6739a
Tae Hoon Lee <thlee1122@hotmail.com>
2017-01-04 00:14:27 -0500

    Fist commit

---
+++ server.js
@@ -0,0 +1,19 @@
+const express = require('express');
+
+// Create our app
+const app = express();
+const PORT = process.env.PORT || 3000;
+
+app.use(function(req, res, next) {
+	if(req.headers['x-forwarded-proto'] === 'http') {
+		res.redirect('http://' + req.hostname + req.url);
+	} else {
+		next();
+	}
+})
+
+app.use(express.static('public'));
+
+app.listen(PORT, function () {
+  console.log('Express server is up on port ' + PORT);
+});
